2fb139dcbdf42318f40e35cb7477aa72cdc4bcb5
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2012-08-18 16:59:07 +0200

    Mac update

--- SA.py
+++ SA.py
@@ -678,7 +678,7 @@ def Utils():
 					ExZip(os.path.join(ConfDir, "IM.tar.gz"), Home, 'tar')
 					if not os.path.join(Home, "ImageMagick-6.7.8", "bin") in PATH:
 						print "yep"
-						msg = '#\nMAGICK_HOME="$HOME/ImageMagick-6.7.9"\nPATH="$MAGICK_HOME/bin:$PATH"\nDYLD_LIBRARY_PATH="$MAGICK_HOME/lib/"'
+						msg = '''#\nMAGICK_HOME='$HOME/ImageMagick-6.7.9'\nPATH='$MAGICK_HOME/bin:'$PATH''\nDYLD_LIBRARY_PATH='$MAGICK_HOME/lib/''''
 						SystemLog('echo "%s" >> %s' %(msg, os.path.join(Home, ".profile")))
 		if OS == 'Win':
 			wait = NewDialog(_(":("),  _("Sorry, windows does not support PATH modifications from cmd...\nInstead, I will open up a site for you"
